f5d20a599f7059a2a50b66cbcd7c54d6d4e70f1b
Archit Gupta <architgupta225@gmail.com>
2023-10-16 18:58:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-http.createServer((req, resp) => {
-    resp.writeHead(200, { 'Content-Type': 'application\json' });
-    resp.write(JSON.stringify({ name: 'Archit Gupta', email: 'architgupta225@gmail.com' }))
-    resp.end();
-}).listen(4500);
+const dirPath = path.join(__dirname, 'crud');
+const filePath = `${dirPath}/apple.txt`
+
+// fs.writeFileSync(filePath, "This is a file")
+
+// fs.readFile(filePath, 'utf8',(error, data) => {
+//     console.log(data);
+// })
+
+// fs.appendFile(filePath, "and file name is apple.txt", (err) => {
+//     if(!err) console.log("file is updated");
+// })
+
+// fs.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
+//     if (!err) console.log("file is updated");
+// })
+
+// fs.unlinkSync(`${dirPath}/fruit.txt`)
